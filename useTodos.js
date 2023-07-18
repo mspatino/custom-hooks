7725335f019f14945282c950ed46a4ed24f957a4
mpatino <mspatino@gmail.com>
2023-07-18 16:23:30 -0300

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+        
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+
+export const useTodos = () => {
+  
+
+        
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);   
+        
+    useEffect(() => {
+        //console.log(todos); 
+        localStorage.setItem('todos',JSON.stringify(todos));
+       
+       }, [todos])
+
+       const handleNewTodo = ( todo ) => {
+        const action = {
+            type : '[TODO] Add Todo',
+            payload: todo,
+        }
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch( {
+            type : '[TODO] Remove Todo',
+            payload: id,
+        } );
+    }
+
+    const handleToggleTodo = ( id ) => {
+        console.log({id});
+        dispatch( {
+            type : '[TODO] Toggle Todo',
+            payload: id,
+        } );   
+    }
+
+  
+    return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    todosCount : todos.length,
+    todosPendingCount: todos.filter(todo =>  !todo.done).length
+    }
+
+}
